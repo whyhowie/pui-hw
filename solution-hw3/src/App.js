e5763926261e801c1b8d7af4dfaa8103701d2d5d
howiewang2 <howiewang@cmu.edu>
2022-09-18 13:41:03 -0400

    Updated hw3

--- solution-hw3/src/App.js
+++ solution-hw3/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './Navbar';
-import Grid from './Grid.js';
+import Grid from './GridItem';
 import CartInfo from './CartBehavior'
 
 function App() {
@@ -8,6 +8,17 @@ function App() {
     <div className="App">
         <Navbar></Navbar>
         <Grid></Grid>
+        {/* 
+            Note: Here I decided to create a Grid component with contents
+            created by looping on a list of Roll objects (see GridItem.js
+            and Products.js).
+            
+            I think this is more convenient than creating multiple Item
+            components here and pass in props for each. In the end we will 
+            probably need to read product information from some database. 
+            
+            Multiple YouTube tutorials used a similar method.
+        */}
     </div>
 
     
